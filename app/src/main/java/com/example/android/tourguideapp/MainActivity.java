cf418e5d43a11c88adf03fa9247acf8514efaa6c
Rif7 <rifraf92@gmail.com>
2018-04-17 22:26:40 +0200

    Creating Navigation Drawer as main way to interact with app TODO: Handle navigation click events

--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.tourguideapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setting Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setTitle(R.string.app_name);
+        actionbar.setHomeAsUpIndicator(R.mipmap.ic_launcher); // TODO
+
+
     }
 }
