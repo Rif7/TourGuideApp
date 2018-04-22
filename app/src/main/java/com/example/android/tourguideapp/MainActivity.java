2179a4f88f0f2dff3c046afffd562d5af955e48a
Rif7 <rifraf92@gmail.com>
2018-04-23 00:12:27 +0200

    Created two initial activities and added menu click handlers

--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.android.tourguideapp;
 
+
+import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SightseeingFragment.OnFragmentInteractionListener,
+        GeneralInformationFragment.OnFragmentInteractionListener {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
 
         // Setting Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -18,8 +30,71 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setTitle(R.string.app_name);
-        actionbar.setHomeAsUpIndicator(R.mipmap.ic_launcher); // TODO
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black); // TODO
 
+        // Adding initial fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.main_fragment,
+                new GeneralInformationFragment()).commit();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    selectDrawerItem(menuItem);
+                    return true;
+                }
+            });
+
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.mnu_general_information:
+                fragmentClass = GeneralInformationFragment.class;
+                break;
+            case R.id.mnu_sightseeing:
+                fragmentClass = SightseeingFragment.class;
+                break;
+            default:
+                fragmentClass = GeneralInformationFragment.class;
+        }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.main_fragment, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
