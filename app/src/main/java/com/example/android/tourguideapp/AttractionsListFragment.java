9feed1cf47b50170244bdd4d420a20b2861f125b
Rif7 <rifraf92@gmail.com>
2018-04-24 23:27:04 +0200

    temp

--- app/src/main/java/com/example/android/tourguideapp/AttractionsListFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/AttractionsListFragment.java
@@ -4,80 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SightseeingFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SightseeingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SightseeingFragment extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class AttractionsListFragment extends android.support.v4.app.Fragment {
+    public static AttractionType attractionType;
     private OnFragmentInteractionListener mListener;
-    Button btn;
 
-    public SightseeingFragment() {
+    public AttractionsListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SightseeingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SightseeingFragment newInstance(String param1, String param2) {
-        SightseeingFragment fragment = new SightseeingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_sightseeing, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_attractions, container, false);
 
         final TouristAttractionAdapter adapter = new TouristAttractionAdapter(getActivity(),
                 TouristAttractionManager.getInstance().getTouristsAttrations(AttractionType.SIGHTSEEING));
 
-        ListView listView = (ListView) rootView.findViewById(R.id.sightseeing_list);
+        TextView listName = (TextView) rootView.findViewById(R.id.list_name);
+        listName.setText(attractionType.toString());
+
+        ListView listView = (ListView) rootView.findViewById(R.id.attractions_list);
         listView.setAdapter(adapter);
-//        listView.setItemsCanFocus(false);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
