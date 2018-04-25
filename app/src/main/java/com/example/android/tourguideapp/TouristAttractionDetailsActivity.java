7f1e24aae19150db78077ede5cc73cb74b31c0e3
Rif7 <rifraf92@gmail.com>
2018-04-25 21:35:39 +0200

    Improved TouristAttractionDetailsActivity layout

--- app/src/main/java/com/example/android/tourguideapp/TouristAttractionDetailsActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/TouristAttractionDetailsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.tourguideapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class TouristAttractionDetailsActivity extends AppCompatActivity {
@@ -11,7 +13,10 @@ public class TouristAttractionDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourist_attraction_details);
 
-        TextView detailsName = (TextView) findViewById(R.id.details_name);
-        detailsName.setText(TouristAttractionManager.getInstance().detailedTouristAttraction.getName());
+        Toolbar toolbar = findViewById(R.id.details_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle(TouristAttractionManager.getInstance().detailedTouristAttraction.getName());
+        actionbar.setDisplayHomeAsUpEnabled(true);
     }
 }
