75d9232269faf29b2456f39377144472ce2ab0e4
Rif7 <rifraf92@gmail.com>
2018-04-26 20:42:10 +0200

    Added save Button

--- app/src/main/java/com/example/android/tourguideapp/TouristAttractionDetailsActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/TouristAttractionDetailsActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -55,6 +58,37 @@ public class TouristAttractionDetailsActivity extends AppCompatActivity {
             View item = (View) findViewById(R.id.details_location);
             item.setVisibility(View.VISIBLE);
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.details_action_bar, menu);
+        MenuItem save = menu.findItem(R.id.action_save);
+        if (TouristAttractionManager.getInstance().detailedTouristAttraction.isSaved()) {
+            save.setIcon(R.drawable.ic_star_saved);
+        } else {
+            save.setIcon(R.drawable.ic_star_unsaved);
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                if (touristAttraction.isSaved()) {
+                    item.setIcon(R.drawable.ic_star_unsaved);
+                    TouristAttractionManager.getInstance().deleteSavedTouristAttractions(touristAttraction);
+                } else {
+                    item.setIcon(R.drawable.ic_star_saved);
+                    TouristAttractionManager.getInstance().addSavedTouristAttractions(touristAttraction);
+                }
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
